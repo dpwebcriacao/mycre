fc70d53d4b715bccf89b837637ae56d90bfe8a38
Pablo Zanata <pablo.zanata@gmail.com>
2021-04-21 23:23:57 -0300

    Form de pessoas

---
+++ includes/form-pessoas.php
@@ -0,0 +1,64 @@
+		<div class="form-group float-end">
+        <a class="btn btn-outline-secondary" href="index.php">HOME</a>
+        <a class="btn btn-secondary" href="pessoas.php">PESSOAS</a>
+        <a class="btn btn-outline-secondary" href="titulos.php">TÍTULOS</a>
+        <a class="btn btn-outline-secondary" href="cre.php">CONTAS A RECEBER</a>
+      </div>
+<main>
+	<section>
+		<a href="index.php">
+			<button class="btn btn-outline-secondary">Voltar</button>
+		</a>
+	</section>
+
+	<h2 class="mt-3">Cadastro de Pessoas</h2>
+
+	<form method="POST">
+
+		<div class="form-group mt-2">
+			<label>Nome Completo:</label>
+			<input type="text" class="form-control" name="nome">
+		</div>
+
+		<div class="form-group mt-2">
+			<label>CPF:</label>
+			<input type="text" class="form-control" name="cpf">
+		</div>
+
+		<div class="form-group mt-2">
+			<label>Data de Nascimento:</label>
+			<input type="text" class="form-control" name="dtnasc">
+		</div>
+
+		<div class="form-group mt-2">
+			<label>Endereço:</label>
+			<input type="text" class="form-control" name="logradouro">
+		</div>
+
+		<div class="form-group mt-2">
+			<label>Número:</label>
+			<input type="text" class="form-control" name="numero">
+		</div>
+
+		<div class="form-group mt-2">
+			<label>Complemento:</label>
+			<input type="text" class="form-control" name="Complemento">
+		</div>
+
+		<div class="form-group mt-2">
+			<label>Bairro:</label>
+			<input type="text" class="form-control" name="bairro">
+		</div>
+
+		<div class="form-group mt-2">
+			<label>Cidade:</label>
+			<input type="text" class="form-control" name="cidade">
+		</div>
+
+		<div class="form-group mt-2">
+			<label>Estado:</label>
+			<input type="text" class="form-control" name="uf">
+		</div>
+
+	</form>
+</main>
---
+++ includes/home.php
@@ -0,0 +1,6 @@
+	<div class="form-group float-end">
+        <a class="btn btn-secondary" href="index.php">HOME</a>
+        <a class="btn btn-outline-secondary" href="pessoas.php">PESSOAS</a>
+        <a class="btn btn-outline-secondary" href="titulos.php">TÍTULOS</a>
+        <a class="btn btn-outline-secondary" href="cre.php">CONTAS A RECEBER</a>
+    </div>
