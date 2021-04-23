700cbf2d889a063f028fd7ffeaa0b81c4af32c76
Pablo Zanata <pablo.zanata@gmail.com>
2021-04-22 23:51:56 -0300

    menu titulos

--- includes/lista-titulos.php
+++ includes/lista-titulos.php
@@ -1,7 +1,7 @@
 <div class="form-group float-end">
     <a class="btn btn-outline-secondary" href="index.php">HOME</a>
-    <a class="btn btn-secondary" href="pessoas.php">PESSOAS</a>
-    <a class="btn btn-outline-secondary" href="titulos.php">TÍTULOS</a>
+    <a class="btn btn-outline-secondary" href="pessoas.php">PESSOAS</a>
+    <a class="btn btn-secondary" href="titulos.php">TÍTULOS</a>
     <a class="btn btn-outline-secondary" href="cre.php">CONTAS A RECEBER</a>
 </div>
 
